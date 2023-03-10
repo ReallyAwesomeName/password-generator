0c25f7c0ea915aaf15a5f548d8d75f1fae229733
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-03-10 18:00:48 -0500

    made input elements - not yet functional

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var configureBtn = document.querySelector("#configure");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,20 +11,60 @@ function writePassword() {
 }
 function generatePassword() {
   var retval;
-  // user Xyz on stackoverflow https://stackoverflow.com/questions/1497481/javascript-password-generator#answer-29770068
-  retval = window.crypto
-    .getRandomValues(new BigUint64Array(4))
-    .reduce(
-      (prev, curr, index) =>
-        (!index ? prev : prev.toString(36)) +
-        (index % 2 ? curr.toString(36).toUpperCase() : curr.toString(36))
-    )
-    .split("")
-    .sort(() => 128 - window.crypto.getRandomValues(new Uint8Array(1))[0])
-    .join("");
-  // end Xyz
+
+  return retval;
+}
+
+function configPopup() {
+  // prompt user for password criteria and return an object to use in generation
+  var retval;
+  var special;
+  var options = [
+    "Number of characters",
+    "Lowercase",
+    "Uppercase",
+    "Numeric",
+    "Special",
+  ];
+  // make unordered list where config options will be placed
+  const ul = document.createElement("ul");
+  const footer = document.getElementById("card-footer");
+  footer.appendChild(ul);
+  // create input/checkboxes for each configuration option
+  for (element of options) {
+    const li = document.createElement("li");
+    li.setAttribute("style", "margin:1em");
+    // num chars needs integer input instead of checkbox
+    if (element === "Number of characters") {
+      const input = document.createElement("input");
+      input.setAttribute("type", "number");
+      input.setAttribute("min", "8");
+      input.setAttribute("max", "128");
+      input.setAttribute("style", "max-width:3.5em");
+      ul.appendChild(li);
+      li.appendChild(input);
+    } else {
+      // make the checkboxes for the rest
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.setAttribute("id", element);
+      li.appendChild(checkbox);
+
+      const label = document.createElement("label");
+      label.setAttribute("for", element);
+      const textContent = document.createTextNode(` ${element}`);
+      label.appendChild(textContent);
+
+      ul.appendChild(li);
+      li.appendChild(label);
+    }
+  }
+
   return retval;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to configure button
+configureBtn.addEventListener("click", configPopup);
